f9d0d649097cb07f611b081c3af2ce89bcfc4459
tsbw <tilmansoerenw@protonmail.com>
2023-12-19 18:59:35 +0100

    safety commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,43 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="Frame">
+    <div data-role="page" className="App">
+
+      <div className="Frame">
         <header className="App-header">
           <div className="AppHeaderInnerDiv">
-            <p className="">Tilman - Sören Bertram</p>
+            <p className="TextSize">Tilman - Sören Bertram</p>
           </div>
           <hr />
-          <img
-            src={require("./ressources/pictureMe.png")}
-            alt="Pictr. of Self"
-            className="resize"
-          />
         </header>
+        <div className="DataDivWrapper">
+          <span className="DataDiv">
+            <img
+              src={require("./ressources/pictureMe.png")}
+              alt="Pictr. of Self"
+              className="resize"
+            />
+            <text>Birth: 05.09.2001</text>
+            <text>Loc: Bavery, Landshut</text>
+            <text>Mail: tilmansoerenw@protonmail.com</text>
+            <a className="link_" href="https://github.com/d2tsb">Github: d2tsb</a>
+          </span>
+        </div>
+        <div className="ListItem1">
+          <ul className="ul">
+            <li>
+              <a href="#home">About</a>
+            </li>
+            <li>
+              <a href="#news">Preferences and Focus</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
