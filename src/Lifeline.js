97623d795c629e8802c1250c368ad9ae550321e4
tsbw <tilmansoerenw@protonmail.com>
2023-12-20 22:19:20 +0100

    edited lifeline

--- src/Lifeline.js
+++ src/Lifeline.js
@@ -1,97 +1,50 @@
 
-import React, { useEffect, useRef, useState } from "react";
-import "./Lifeline.css"
-
-import TimelineObserver from "react-timeline-animation";
+import React, { useState } from "react";
+import HorizontalTimeline from "react-horizontal-timeline";
 
 
-const Timeline = ({ setObserver, callback }) => {
-  const [message1, setMessage1] = useState("");
-  const [message2, setMessage2] = useState("");
-  const [message3, setMessage3] = useState("");
 
-  const timeline1 = useRef(null);
-  const timeline2 = useRef(null);
-  const timeline3 = useRef(null);
-  const circle1 = useRef(null);
-  const circle2 = useRef(null);
-  const circle3 = useRef(null);
+const Lifeline = () => {
 
-  const someCallback = () => {
-    setMessage1("Step one");
-    callback();
-  };
 
-  const someCallback2 = () => {
-    setMessage2("Step two");
-  };
+    const [value, setValue] = useState(0);
+    const [previous, setPrevious] = useState(0);
+ 
+    // Values should be only date
+    const VALUES = ["09.05.2001", "2021-01-15", "2021-03-22"];
+ 
+    // Description array corresponding to values
+    const description = [
+        "Birth",
+        "",
+        "The event of 22 March 2021 : Board Exam",
+    ];
+ 
 
-  const someCallback3 = () => {
-    setMessage3("Finish");
-  };
 
-  useEffect(() => {
-    setObserver(timeline1.current);
-    setObserver(timeline2.current);
-    setObserver(timeline3.current);
-    setObserver(circle1.current, someCallback);
-    setObserver(circle2.current, someCallback2);
-    setObserver(circle3.current, someCallback3);
-  }, []);
+
 
   return (
-    <div className="wrapper">
-      <div id="timeline1" ref={timeline1} className="timeline" />
-      <div className="circleWrapper">
-        <div id="circle1" ref={circle1} className="circle">
-          1
+
+     <div className="root-div">
+            <div style={{
+                width: "60%",
+                height: "100px",
+                margin: "0 auto"
+            }}>
+                <HorizontalTimeline
+                    styles={{ outline: "#000", foreground: "#FFF" }}
+                    index={value}
+                    indexClick={(index) => {
+                        setValue(index);
+                        setPrevious(value);
+                    }} 
+                    values={VALUES}
+                />
+            </div>
+            <div className="" style={{rotate:"-30deg", margin:"0 0"}}>{description[value]}</div>
         </div>
-        <div className="message">{message1}</div>
-      </div>
-      <div id="timeline2" ref={timeline2} className="timeline" />
-      <div className="circleWrapper">
-        <div id="circle2" ref={circle2} className="circle">
-          2
-        </div>
-        <div className="message">{message2}</div>
-      </div>
-      <div id="timeline3" ref={timeline3} className="timeline" />
-      <div className="circleWrapper">
-        <div id="circle3" ref={circle3} className="circle">
-          3
-        </div>
-        <div className="message">{message3}</div>
-      </div>
-    </div>
-  );
-};
+  )
+}
 
-export default function Lifeline () {
-  const [message, setMessage] = useState("");
-
-  const onCallback = () => {
-    console.log("awesome");
-  };
-
-  return (
-    <div style={ {background: '#222222', rotate: '270deg' } } >
-      <h1>react-scroll-animation component</h1>
-      <TimelineObserver
-        initialColor="#e5e5e5"
-        fillColor="black"
-        handleObserve={(setObserver) => (
-          <Timeline
-            callback={onCallback}
-            className="timeline"
-            setObserver={setObserver}
-          />
-        )}
-      />
-      <div className="stub2">{message}</div>
-    </div>
-  );
-
-
-
-
-};
+export default Lifeline
