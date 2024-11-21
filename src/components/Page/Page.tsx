c7791734eea0f9b2a3da81531be875a4b0494d7e
dxdye <tilman.bertram@campudus.com>
2024-11-21 22:59:34 +0100

    refactor: split App into components refactor: update readme

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,44 @@
+import Texts from "../Texts/Texts";
+import LightDarkSwitch from "../LightDarkSwitch/LightDarkSwitch";
+import GithubCrawler from "../GithubCrawler/GithubCrawler";
+import { BottomBar } from "../BottomBar/BottomBar";
+import CopyRight from "../CopyRight/CopyRight";
+import Profile from "../Profile/Profile";
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import { image_map } from "../../resources/image_source/image_map";
+const Page = () => {
+  const [Language, setLanguage] = useState(0);
+  const [colorTheme, setColorTheme] = useState(0); //0 is dark, 1 is high.
+  const GIF = (
+    <div className="styleGIFPic">
+      <img //unused
+        id="styleGIF"
+        className="styleGIF"
+        alt="G I F"
+        src={image_map.topBanner}
+      ></img>
+    </div>
+  );
+
+  return (
+    <div data-role="page" className="App">
+      <div
+        className="Frame"
+      >
+        <Header
+          language={Language}
+          setLanguage={setLanguage}
+          DM={{ colorTheme, setColorTheme }}
+        />
+        {GIF}
+        <Profile language={Language} />
+        <Texts language={Language} />
+        <GithubCrawler />
+        <BottomBar language={Language} />
+        <CopyRight />
+      </div>
+    </div>
+  );
+};
+export default Page; 
