354216382364f4b782dac1a8ee9afd26416c0385
dxdye <tilman.bertram@campudus.com>
2024-12-18 21:29:07 +0100

    refactor: allow ts imports

---
+++ src/__resources__/structure.ts
@@ -0,0 +1,9 @@
+export const MenuBarOptions = {
+  en: ["About", "Preferences and Focus", "History"],
+  de: ["Übersicht", "Fokus", "Werdegang"],
+};
+
+export const BottomBarOptions = {
+  en: "Legals",
+  de: "Impressum",
+};
