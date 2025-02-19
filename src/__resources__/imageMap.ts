5a028abe89da6f5b411d984c4d4d5b3fbccbe033
dxdye <s-tbertr@haw-landshut.de>
2025-02-19 01:45:52 +0100

    refactor: add submit button to menu

--- src/__resources__/imageMap.ts
+++ src/__resources__/imageMap.ts
@@ -1,12 +1,13 @@
 const buildImgPath = (imgName: string) => `/images/${imgName}`;
 export const imageMap = {
+  topBanner: buildImgPath("TopBanner.gif"),
+  me: buildImgPath("Me.jpg"),
   campudusLogo: buildImgPath("CampudusLogo.png"),
   codeBackground: buildImgPath("CodeBackground.png"),
   githubLogo: buildImgPath("GitHubLogo.png"),
   githubLogoTransparent: buildImgPath("GitHubLogoTransparent.png"),
-  linkedInLogo: buildImgPath("LinkedInLogo.svg"),
   lightLogo: buildImgPath("LightLogo.png"),
-  me: buildImgPath("Me.jpg"),
+  linkedInLogo: buildImgPath("LinkedInLogo.svg"),
+  submit: buildImgPath("greenArrow.svg"),
   menu: buildImgPath("menu.svg"),
-  topBanner: buildImgPath("TopBanner.gif"),
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,7 @@
-import { useContext } from "react";
-import { PageContext } from "../Page/Page";
 import "./Header.scss";
-import { Language } from "../../__resources__/types";
 import MenuSwitch from "./MenuSwitch/MenuSwitch";
 
 const Header = () => {
-  const { language, setLanguage } = useContext(PageContext);
-
   const title = "Personal Homepage";
   return (
     <header className="header">
