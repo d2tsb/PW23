d3233e0a1949e50b9b82d7fcec7cf2b257a58794
dxdye <s-tbertr@haw-landshut.de>
2025-02-25 17:15:04 +0100

    feat: add sandwich animation

--- src/components/Header/MenuSwitch/MenuSwitch.tsx
+++ src/components/Header/MenuSwitch/MenuSwitch.tsx
@@ -1,21 +1,25 @@
 import "./MenuSwitch.scss";
 import { useContext } from "react";
-import { imageMap } from "../../../__resources__/imageMap";
 import { PageContext } from "../../Page/Page";
 
-const Sandwich = () => (
-  <div className="sandwich">
-    <div className="sandwich-element" />
-    <div className="sandwich-element" />
-    <div className="sandwich-element" />
-  </div>
-);
+const Sandwich = ({ showCross }: { showCross?: boolean }) => {
+  const crossTop = showCross ? "sandwich-element--top" : "";
+  const crossMiddle = showCross ? "sandwich-element--middle" : "";
+  const crossBottom = showCross ? "sandwich-element--bottom " : "";
+  return (
+    <div className="sandwich">
+      <div className={"sandwich-element " + crossTop} />
+      <div className={"sandwich-element " + crossMiddle} />
+      <div className={"sandwich-element " + crossBottom} />
+    </div>
+  );
+};
+
 const MenuSwitch = () => {
   const { setShowMenu, showMenu } = useContext(PageContext);
-  const selected = showMenu ? "menu-switch--selected" : "";
   return (
-    <div onClick={() => setShowMenu(true)} className={selected}>
-      <Sandwich />
+    <div onClick={() => setShowMenu(true)}>
+      <Sandwich showCross={showMenu} />
     </div>
   );
 };
