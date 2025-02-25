d5a90067e6b9c705caf7e1cfea07c3c422d1d38f
dxdye <s-tbertr@haw-landshut.de>
2025-02-25 16:59:32 +0100

    refactor: replace sandwich menu

--- src/components/Header/MenuSwitch/MenuSwitch.tsx
+++ src/components/Header/MenuSwitch/MenuSwitch.tsx
@@ -3,16 +3,19 @@ import { useContext } from "react";
 import { imageMap } from "../../../__resources__/imageMap";
 import { PageContext } from "../../Page/Page";
 
+const Sandwich = () => (
+  <div className="sandwich">
+    <div className="sandwich-element" />
+    <div className="sandwich-element" />
+    <div className="sandwich-element" />
+  </div>
+);
 const MenuSwitch = () => {
   const { setShowMenu, showMenu } = useContext(PageContext);
   const selected = showMenu ? "menu-switch--selected" : "";
   return (
-    <div onClick={() => setShowMenu(true)}>
-      <img
-        src={imageMap.menu}
-        className={"menu-switch " + selected}
-        alt="sandwich"
-      />
+    <div onClick={() => setShowMenu(true)} className={selected}>
+      <Sandwich />
     </div>
   );
 };
