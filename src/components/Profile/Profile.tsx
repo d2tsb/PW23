7b2334e6d8a8a9d60ba5143363a068a5871c911d
dxdye <tilman.bertram@campudus.com>
2025-10-03 17:05:32 +0200

    refactor: profile logo section now more generic

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,21 +1,21 @@
-import "./Profile.scss";
-import { useContext } from "react";
-import { PageContext } from "../Page/Page";
+import './Profile.scss';
+import { useContext } from 'react';
+import { PageContext } from '../Page/Page';
 import {
   ProfilePicture,
   ProfileInfoMap,
   ProfileLogoSection,
   accumulateInfos,
-} from "./ProfileInfo/ProfileInfo";
+} from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
   const { language, year } = useContext(PageContext);
   const infos = accumulateInfos(year, language);
   return (
-    <div className="profile">
-      <div className="profile__content">
+    <div className='profile'>
+      <div className='profile__content'>
         <ProfilePicture />
-        <ProfileLogoSection language={language} />
+        <ProfileLogoSection language={language} year={year} />
         <ProfileInfoMap infos={infos} />
       </div>
     </div>
