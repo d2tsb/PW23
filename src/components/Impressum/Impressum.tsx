26c446980d2dc454b3509eaad5ce97c94bf9b7de
dxdye <s-tbertr@haw-landshut.de>
2025-10-05 00:28:33 +0200

    feat: remaining style tags only in Impressum Lifeline and ProfileInfo

--- src/components/Impressum/Impressum.tsx
+++ src/components/Impressum/Impressum.tsx
@@ -1,18 +1,15 @@
-import "./Impressum.scss";
+import './Impressum.scss';
 const Impressum = () => {
   return (
     <div>
-      <div style={{ color: "#FFF", height: "70px" }}>Impressum</div>
-      <ul className="impressum">
-        <li style={{ color: "#FFF" }}>
-          reactjs: https://opensource.fb.com/legal/terms or opensource@meta.com{" "}
+      <div style={{ color: '#FFF', height: '70px' }}>Impressum</div>
+      <ul className='impressum'>
+        <li style={{ color: '#FFF' }}>
+          reactjs: https://opensource.fb.com/legal/terms or opensource@meta.com
         </li>
-        <li style={{ color: "#FFF" }}>
-          linkedin: https://www.linkedin.com/legal/impressum
-        </li>
-        <li style={{ color: "#FFF" }}>
-          giphy:
-          https://support.giphy.com/hc/en-us/sections/360003012792-Privacy-and-Safety
+        <li style={{ color: '#FFF' }}>linkedin: https://www.linkedin.com/legal/impressum</li>
+        <li style={{ color: '#FFF' }}>
+          giphy: https://support.giphy.com/hc/en-us/sections/360003012792-Privacy-and-Safety
         </li>
       </ul>
     </div>
